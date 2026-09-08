6ab2b7a2f72b0dcf7fa1d632e02d17e30ae8c1d8
Piyush Mehrotra <piyushmehrotra61@gmail.com>
2026-09-08 23:12:59 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (57.08%) - LeetHub

--- Easy/3870-count-commas-in-range/3870-count-commas-in-range.java
+++ Easy/3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,14 @@
 class Solution {
     public int countCommas(int n) {
+
+        return (n >= 1000) ? (n - 999) : 0;
         
-        int cnt = 0;
-        for(int i=1; i<= n; i++){
-            if(i >= 1000){
-                cnt ++;
-            }
-        }
-        return cnt;
+        // int cnt = 0;
+        // for(int i=1; i<= n; i++){
+        //     if(i >= 1000){
+        //         cnt ++;
+        //     }
+        // }
+        // return cnt;
     }
 }
